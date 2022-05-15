1ff2bb85abfb8c4a477f550d08c47fe5f4cba7d8
paikar2022 <n1kapa00@students.oamk.fi>
2022-05-15 09:55:35 +0300

    typos checked

--- sivustotieto.js
+++ sivustotieto.js
@@ -1,12 +1,11 @@
 //fetch
 //this is just testing to see if I get this working
-
 fetch('https://yesno.wtf/api')
     .then(response => response.json())
     .then(
         data => {
             document.getElementById("printHere").innerHTML 
-            ="<h2>" + "Should I study more about REST API? Answer is: "+ data.answer + "</h2>";
+            ="<h2>" + "I really think I should study more about REST API? Do you agree? : "+ data.answer + "</h2>";
             document.getElementById("printHere").innerHTML 
             +=' <img src= " ' + data.image + ' " /> '; 
         }
